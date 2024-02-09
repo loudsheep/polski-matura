17913e0af28f95e344e7e8d19442048c2d5e8129
loudsheep <loudsheep05@gmail.com>
2024-02-09 17:55:31 +0100

    Fixed markdown

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Head from 'next/head';
 import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
