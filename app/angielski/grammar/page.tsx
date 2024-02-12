547d69f007fdc22db046a61d963fd1d4a7b7ae99
Bartix3021 <bartekfopke@gmail.com>
2024-02-12 20:57:31 +0100

    adding more pages

--- app/angielski/grammar/page.tsx
+++ app/angielski/grammar/page.tsx
@@ -18,7 +18,7 @@ export default function Grammar_Page() {
 
     return (
         <div className="flex">
-            <div className="fixed left-0 top-0 h-full w-48 bg-gray-800 text-white flex flex-col items-center  z-50">
+            <div className="fixed left-0 top-0 h-full w-48 bg-gray-800 text-white flex flex-col items-center z-50">
                 <h2 className="text-xl font-bold mb-4">Tematy</h2>
                 <button style={{ width: '90%' }} className="text-white px-4 py-2 mb-2 bg-blue-600 hover:bg-blue-700 rounded" onClick={() => handleClick(1)}>Simple & Continuous Forms</button>
                 <button style={{ width: '90%' }} className="text-white px-4 py-2 mb-2 bg-blue-600 hover:bg-blue-700 rounded" onClick={() => handleClick(2)}>Narrative Tenses</button>
