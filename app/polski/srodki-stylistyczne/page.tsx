c5c0a9c58f0244aa0a2a6e90a06e3192eef1a499
loudsheep <loudsheep05@gmail.com>
2024-02-12 09:55:03 +0100

    Changed wrong page header

--- app/polski/srodki-stylistyczne/page.tsx
+++ app/polski/srodki-stylistyczne/page.tsx
@@ -21,7 +21,7 @@ export default function SrodkiPage() {
 
     return (
         <div className='w-11/12 md:w-5/6 lg:w-3/4 xl:w-1/2 mx-auto flex-col items-center'>
-            <h1 className='text-center text-3xl font-bold my-10'>Epoki literackie w skrócie</h1>
+            <h1 className='text-center text-3xl font-bold my-10'>Środki literackie</h1>
 
             {jsonData && jsonData.map((value: any, idx: any) => (
                 <div key={idx} className='mb-10 pb-5 border-b'>
