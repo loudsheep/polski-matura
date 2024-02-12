b692ef52f26b8c7357ef003a9823a7273078f26b
Bartix3021 <bartekfopke@gmail.com>
2024-02-12 20:57:13 +0100

    adding more pages

--- app/angielski/grammar/page.tsx
+++ app/angielski/grammar/page.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import UnitOne from "./unit_1";
 import UnitTwo from "./unit_2";
+import UnitThree from "./unit_3";
+import UnitFour from "./unit_4";
 
 export default function Grammar_Page() {
     const [clickedButton, setClickedButton] = useState<number | null>(null);
@@ -16,11 +18,12 @@ export default function Grammar_Page() {
 
     return (
         <div className="flex">
-            <div className="fixed left-0 top-0 h-full w-48 bg-gray-800 text-white flex flex-col items-center justify-center z-50">
+            <div className="fixed left-0 top-0 h-full w-48 bg-gray-800 text-white flex flex-col items-center  z-50">
                 <h2 className="text-xl font-bold mb-4">Tematy</h2>
                 <button style={{ width: '90%' }} className="text-white px-4 py-2 mb-2 bg-blue-600 hover:bg-blue-700 rounded" onClick={() => handleClick(1)}>Simple & Continuous Forms</button>
                 <button style={{ width: '90%' }} className="text-white px-4 py-2 mb-2 bg-blue-600 hover:bg-blue-700 rounded" onClick={() => handleClick(2)}>Narrative Tenses</button>
-                <button style={{ width: '90%' }} className="text-white px-4 py-2 mb-2 bg-blue-600 hover:bg-blue-700 rounded" onClick={() => handleClick(3)}>Button 3</button>
+                <button style={{ width: '90%' }} className="text-white px-4 py-2 mb-2 bg-blue-600 hover:bg-blue-700 rounded" onClick={() => handleClick(3)}>Future Forms</button>
+                <button style={{ width: '90%' }} className="text-white px-4 py-2 mb-2 bg-blue-600 hover:bg-blue-700 rounded" onClick={() => handleClick(4)}>Verb patterns & modals</button>
             </div>
             <div className="flex-grow ml-48 p-8">
                 {clickedButton == null && (
@@ -31,6 +34,14 @@ export default function Grammar_Page() {
                 )}
                 {clickedButton == 2 && (
                     <UnitTwo></UnitTwo>
+                )}
+
+                {clickedButton == 3 && (
+                    <UnitThree></UnitThree>
+                )}
+
+                {clickedButton == 4 && (
+                    <UnitFour></UnitFour>
                 )}
             </div>
         </div>
---
+++ app/angielski/grammar/unit_5.tsx
@@ -0,0 +1,3 @@
+export default function UnitFive() {
+    return (<div></div>)
+}
