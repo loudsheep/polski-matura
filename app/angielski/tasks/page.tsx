4d9d254911e23f73ba9457db291dd750d8431044
Bartix3021 <bartekfopke@gmail.com>
2024-02-15 17:27:45 +0100

    working on the english test

--- app/angielski/tasks/page.tsx
+++ app/angielski/tasks/page.tsx
@@ -1,27 +1,25 @@
+"use client"
 import { useEffect, useState } from "react";
 
-interface Data {
-    
-}
+
 export default function Tasks_Page() {
-    const [jsonData, setJsonData] = useState<Data | null>(null);
+    const [jsonData, setJsonData] = useState<any | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
-          try {
-            const response = await fetch('/files/data.json'); // Path to your JSON file
-            if (!response.ok) {
-              throw new Error('Failed to fetch data');
+            try {
+                const response = await fetch('/data/matura_files/reading_tasks.json');
+                const data = await response.json();
+                console.log(data);
+                setJsonData(data);
+            } catch (error) {
+                console.error('Error fetching JSON data:', error);
             }
-            const data: Data = await response.json();
-            setJsonData(data);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
         };
     
         fetchData();
       }, []);
+      
     return (
         <div></div>
     )
