34bd55dd5642f147d5b6372e14a61a6d472fe66d
Pepe <stockipiotrek1@gmail.com>
2024-02-18 14:45:53 +0100

    siema, ja po creditsy
    dodałem jedno słowo, pozdrawiam

--- app/polski/page.tsx
+++ app/polski/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function PolskiPage() {
     return (
         <div className="w-3/4 mx-auto flex flex-col items-center">
-            <h1 className="text-2xl font-bold my-10">Strona główna do matury z polskiego</h1>
+            <h1 className="text-2xl font-bold my-10">Strona główna do matury z języka polskiego</h1>
 
             <div className="w-1/2">
                 <ul className="list-disc">
