08c92c559761649b7cf37c9623b41555fd1a3679
loudsheep <loudsheep05@gmail.com>
2024-02-19 21:01:49 +0100

    Fixed hydration error on TimeLeft component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import TimeLeft from '@/components/TimeLeft'
-import Link from 'next/link'
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+
+const TimeLeft = dynamic(() => import("@/components/TimeLeft"), {
+    ssr: false,
+});
 
 export default function Home() {
     return (
-        <div className="flex min-h-screen flex-col items-center p-24">
+        <div className="w-11/12 md:w-5/6 lg:w-3/4 xl:w-1/2 mx-auto flex-col items-center pt-12">
             <h1 className="text-2xl font-bold mb-10">Strona główna strony z przygotowaniem do matury</h1>
 
-            <div className="w-full md:w-1/2">
+            <div className="w-full md:w-1/2 pl-2">
                 <ul className="list-disc">
                     <li>
                         <Link href={"/polski"} className="text-blue-500">J. polski</Link>
