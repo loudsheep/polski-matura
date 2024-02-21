a5a20da63b95230f41d978bd94640151a0f94843
Qufel <97023524+Qufel@users.noreply.github.com>
2024-02-21 16:52:54 +0100

    Hamburger Menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import React from 'react';
+import MobileNav from '@/components/MobileNav';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,7 +39,7 @@ export default function RootLayout({
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
-                        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <div className="hidden w-full lg:block md:w-auto" id="navbar-default">
                             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
                                 <li>
                                     <a href="/informatyka" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Informatyka</a>
@@ -54,6 +55,7 @@ export default function RootLayout({
                                 </li>
                             </ul>
                         </div>
+                        <MobileNav></MobileNav>
                     </div>
                 </nav>
 
